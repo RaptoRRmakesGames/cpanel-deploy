07d7deaf25410d015d403b782e3e07d56fd88dcc
RaptoRRmakesGames <gjkafantaris@gmail.com>
2023-12-28 20:32:18 +0200

    INIT COMIT

---
+++ app.py
@@ -0,0 +1,16 @@
+# Import the Flask class from the flask module
+from flask import Flask, render_template
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Define a route and a view function
+@app.route('/')
+def hello():
+    return render_template("index.html")
+
+
+    
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
