b32f691570fdc44e7ac2eb3542a241328b911ccb
RaptoRRmakesGames <gjkafantaris@gmail.com>
2024-07-19 18:13:59 +0300

    fwaeh

--- db.py
+++ db.py
@@ -691,7 +691,10 @@ class KitchenGroup:
                     self.remove_employee_from_current_department(em)
                     # em.pass_to_department(self.get_department_by_name(dept, kitchen), emp[1])
                     print('fwaeh: ',dept, kitchen)
-                    self.get_department_by_name(dept, kitchen).employees.append((em, emp[1]))
+                    try:
+                        self.get_department_by_name(dept, kitchen).employees.append((em, emp[1]))
+                    except AttributeError:
+                        print('sum weird sh happning')
         return emps_added
 
     def __repr__(self) -> str:
