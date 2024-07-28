be71ac34dbc10b9d666437b8dabbabdf3271c205
RaptoRRmakesGames <gjkafantaris@gmail.com>
2024-07-28 20:15:06 +0300

    fwaeh

--- db.py
+++ db.py
@@ -50,7 +50,7 @@ def get_subdept(ide):
     try:
         db,c = connect(); c.execute('SELECT * FROM sub_department WHERE id=%s', [ide]); return Department(c.fetchall()[0][0])
     except Exception as e:
-        print('Error: ' + str(e))
+        print('Error: ' , str(e))
     
 def create_title(name):
     db,c = connect()
@@ -744,8 +744,12 @@ class KitchenGroup:
         
         for kitch in self.sub_kitchens:
             for dep in kitch.sub_departments:
-                if employee in dep.employees:
-                    dep.employees.remove(employee)
+                try:
+                    
+                    if employee in dep.employees:
+                        dep.employees.remove(employee)
+                except Exception as e:
+                    print('Error:', e)
                     
     def remove_duplicates(self):
         
